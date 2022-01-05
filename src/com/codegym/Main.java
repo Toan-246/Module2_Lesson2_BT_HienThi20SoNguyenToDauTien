c12a7b615306fe7dbd0bf7aada38391e080d825a
Toan-246 <n.v.toan687@gmail.com>
2022-01-05 15:45:29 +0700

    add

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,42 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = 0;
+        int count = 0;
+
+        while (count < 20) {
+
+            if (number < 2) {
+                number++;
+                continue;
+            }
+//            else if (number == 2) {
+//                System.out.println(number);
+//                count++;
+//            }
+            else {
+                boolean isPrime = true;
+                for (int i = 2; i <= Math.sqrt(number); i++) {
+                    if (number % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) {
+                    count++;
+                    System.out.println(number);
+                }
+
+            }
+            number++;
+        }
+
+
+    }
+
+}
